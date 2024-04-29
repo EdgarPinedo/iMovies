6ae908b0a352c843fb543b956ea7820141435688
EdgarPinedo <49216133+EdgarPinedo@users.noreply.github.com>
2024-04-29 16:52:01 -0600

    Update CustomHeaderHandler.cs

--- MoviesApp/HttpHandlers/CustomHeaderHandler.cs
+++ MoviesApp/HttpHandlers/CustomHeaderHandler.cs
@@ -9,7 +9,7 @@ namespace MoviesApp.HttpHandlers
         protected override Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            string jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MTJhNTk0ZGY5NDIxYjFmMzM0YTFiZWE1ZmZkN2E4OSIsInN1YiI6IjY1OWM0N2VjMmRjNDRlMDFmY2E5NmY0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kqY0PVD7KyQIvczu2bGLbQj6aQ7vc9KBzx7Yh1oB5J0";
+            string jwtToken = "my_api_token";
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
             return base.SendAsync(request, cancellationToken);
         }
